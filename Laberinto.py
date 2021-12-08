e874506e62d9bd745c0bf7379120f830102bb354
crltsnch <crltsnch@myuax.com>
2021-12-08 19:32:51 +0100

    Crear funcion laberinto

--- Laberinto.py
+++ Laberinto.py
@@ -6,4 +6,17 @@ laberinto = [
 ['X', 'X', 'X', 'X', 'S']
 ]
 
+def laberinto(dimension, muro):
+    laberinto=[]
+    for i in range(dimension):
+        fila=[]
+    for j in range(dimension):
+        columna=[]
+        if (i, j) in muro:
+            fila.append("x")
+        else:
+            fila.append(" ")
+            
+    return(laberinto)
+
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
