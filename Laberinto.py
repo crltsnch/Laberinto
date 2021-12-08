0e4f947a37883c714d0c554c7d0f1b4558878a2f
crltsnch <crltsnch@myuax.com>
2021-12-08 19:44:59 +0100

    Funcion laberinto ¿"bien"?

--- Laberinto.py
+++ Laberinto.py
@@ -10,13 +10,13 @@ def laberinto(dimension, muro):
     laberinto=[]
     for i in range(dimension):
         fila=[]
-    for j in range(dimension):
-        columna=[]
-        if (i, j) in muro:
-            fila.append("x")
-        else:
-            fila.append(" ")
-            
+        for j in range(dimension):
+            if (i, j) in muro:
+                fila.append("x")
+            else:
+                fila.append(" ")
+        laberinto.append(fila)
     return(laberinto)
+
 
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
